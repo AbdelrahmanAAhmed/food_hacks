d722c5f1e3c8f49d29ab44f8c518c455a277d114
AbdelrahmanAahmed <abdelrahmanabdelhameed2000@gmail.com>
2023-12-25 07:42:56 +0200

    add Bottom Bar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:food_hacks/Widgets/custom_category.dart';
 import 'package:food_hacks/models/foodmodel/categorymodel.dart';
 import 'package:food_hacks/services/api.service.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -30,14 +31,34 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          bottomNavigationBar: const GNav(
+            backgroundColor: Colors.amber,
+            color: Colors.white,
+            activeColor: Colors.white,
+            gap: 7,
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.favorite,
+                text: 'Likes',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+            ],
+          ),
           body: ListView.builder(
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          return CustomCategoryListItem(
-            categoriesModel: categories[index],
-          );
-        },
-      )),
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              return CustomCategoryListItem(
+                categoriesModel: categories[index],
+              );
+            },
+          )),
     );
   }
 }
